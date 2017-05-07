efcc7ca023f42577f401fc688aad883673c0a218
Chris Lerum <chris.lerum@gmail.com>
2017-05-07 14:58:41 -0400

    added second module, add home link to modules index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/mississippi_burning', :to => redirect('modules/mississippi_burning/index.html')
+  get '/gemini_landing', :to => redirect('modules/gemini_landing/index.html')
 end
